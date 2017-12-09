c19c67720679e4981117c80c6597db6b741e5f34
Aliaksei Charnevich <liaksiejka@gmail.com>
2017-12-09 23:04:06 +0300

    Add project

---
+++ app/src/main/java/game/aliaksei/yesnogameapp/MainActivity.java
@@ -0,0 +1,41 @@
+package game.aliaksei.yesnogameapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_start).setOnClickListener(this);
+        findViewById(R.id.button_settings).setOnClickListener(this);
+        findViewById(R.id.button_exit).setOnClickListener(this);
+        findViewById(R.id.button_help).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.button_start:
+                // start game;
+                break;
+            case R.id.button_settings:
+                // settings;
+                break;
+            case R.id.button_help:
+                Intent intent = new Intent(this, HelpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_exit:
+                // exit program;
+                break;
+        }
+    }
+}
